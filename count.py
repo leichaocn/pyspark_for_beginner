f866d4460d4722c04212d66f792106ecbe9a7abd
Lei Chao <leichaocn@163.com>
2019-07-24 10:56:40 +0800

    Create count.py

---
+++ count.py
@@ -0,0 +1,25 @@
+# count.py
+# 执行spark-submit count.py，也将会输出结果
+try:
+    sc.stop()  
+except:
+    pass 
+    
+from pyspark import SparkContext
+sc = SparkContext("local", "count app")
+
+# 下面这句，是创建RDD
+words = sc.parallelize(
+    ["scala",
+     "java",
+     "hadoop",
+     "spark",
+     "akka",
+     "spark vs hadoop",
+     #"pyspark",
+     "pyspark and spark"
+     ])
+     
+counts = words.count()
+print("Number of elements in RDD -> %i" % counts)
+sc.stop()
